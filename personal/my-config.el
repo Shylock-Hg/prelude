9abb3556341088a767cca5a9f721c01f065c7eec
Shylock Hg <tcath2s@icloud.com>
2025-11-18 16:06:52 +0800

    feat: enable emacs server

--- personal/my-config.el
+++ personal/my-config.el
@@ -2,4 +2,10 @@
 
 (set-face-attribute 'default nil :font "JetBrains Mono-20")
 
+
+;; auto run emacs server
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
 (provide 'my-config)
