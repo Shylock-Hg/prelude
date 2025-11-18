6bcbe53a2a7e5685df439f81c863a4c7a797a6dc
Shylock Hg <tcath2s@icloud.com>
2025-11-18 17:48:46 +0800

    feat: add typst lsp

---
+++ personal/my-typst.el
@@ -0,0 +1,18 @@
+;;; -*- lexical-binding: t -*-
+
+(prelude-require-package 'typst-ts-mode)
+
+(require 'my-eglot)
+
+;; require tinymist typst-lsp in PATH
+
+;; tinymist
+(with-eval-after-load 'eglot
+  (with-eval-after-load 'typst-ts-mode
+    (add-to-list 'eglot-server-programs
+                  `((typst-ts-mode) .
+                    ,(eglot-alternatives `(,typst-ts-lsp-download-path
+                                          "tinymist"
+                                          "typst-lsp"))))))
+
+(provide 'my-typst)
