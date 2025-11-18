34aa11ad0caf33435a588747958800e3ed0eb249
Shylock Hg <tcath2s@icloud.com>
2025-11-18 18:48:53 +0800

    feat: config github copilot

---
+++ personal/my-github-copilot.el
@@ -0,0 +1,15 @@
+;;; -*- lexical-binding: t -*-
+
+(prelude-require-package 'copilot)
+
+;; M-x copilot-install-server
+;; M-x copilot-login
+;; M-x copilot-mode to enable copilot
+
+;; github copilot
+(global-set-key (kbd "C-<return>") 'copilot-accept-completion)
+
+;; It's unique for each buffer, so we enable it in prog-mode-hook
+(add-hook 'prog-mode-hook 'copilot-mode)
+
+(provide 'my-github-copilot)
