74c14001e50682d98477735f288919c34c2fd453
Shylock Hg <tcath2s@icloud.com>
2025-11-20 20:36:36 +0800

    feat: change theme to ef-day

--- personal/my-modules.el
+++ personal/my-modules.el
@@ -5,7 +5,7 @@
 (require 'my-evil-magit)
 (require 'my-github-copilot)
 (require 'my-lsp-clangd)
-(require 'my-theme-solarized-light)
+(require 'my-theme-ef-day)
 (require 'my-treemacs)
 (require 'my-typst)
 (require 'my-vterm)
