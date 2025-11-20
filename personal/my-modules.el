487534919e32f23ae0ac96aa5fca6a34acae9d00
Shylock Hg <tcath2s@icloud.com>
2025-11-20 17:21:53 +0800

    feat: load all my-modules to one from one entry file

---
+++ personal/my-modules.el
@@ -0,0 +1,11 @@
+;;; -*- lexical-binding: t -*-
+
+(require 'my-config)
+(require 'my-eglot)
+(require 'my-evil-magit)
+(require 'my-github-copilot)
+(require 'my-lsp-clangd)
+(require 'my-theme-solarized-light)
+(require 'my-treemacs)
+(require 'my-typst)
+(require 'my-vterm)
