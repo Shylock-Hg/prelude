b312ca99c46637770380eac845cb5e3c1a756e7a
Shylock Hg <tcath2s@icloud.com>
2025-11-21 17:39:16 +0800

    fix: treemacs lose session state.

--- personal/my-modules.el
+++ personal/my-modules.el
@@ -1,12 +1,14 @@
 ;;; -*- lexical-binding: t -*-
 
+(require 'custom)
+
 (require 'my-config)
 (require 'my-eglot)
 (require 'my-evil-magit)
 (require 'my-github-copilot)
 (require 'my-lsp-clangd)
 (require 'my-theme-ef-day)
-(require 'my-treemacs)
 (require 'my-typst)
 (require 'my-vterm)
+(require 'my-treemacs)
 (require 'my-lsp-rust)
