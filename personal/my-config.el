e43128ddad4deda131272a6813c38008b09b73ea
Shylock Hg <tcath2s@icloud.com>
2025-11-21 21:35:42 +0800

    fix: emacs company candidates order.

--- personal/my-config.el
+++ personal/my-config.el
@@ -12,5 +12,9 @@
 ;; key bindg emacs command
 (global-set-key (kbd "C-i") 'execute-extended-command)
 
+(with-eval-after-load 'company
+  (setq company-tooltip-flip-when-above nil)
+  )
+
 
 (provide 'my-config)
