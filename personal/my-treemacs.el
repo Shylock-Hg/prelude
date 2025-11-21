1c0ac6ef6c5bec6ef3be133b7dd00e8fd1d13409
Shylock Hg <tcath2s@icloud.com>
2025-11-21 17:39:16 +0800

    fix: treemacs lose session state.

--- personal/my-treemacs.el
+++ personal/my-treemacs.el
@@ -2,7 +2,7 @@
 
 (require 'prelude-packages)
 
-(prelude-require-packages '(treemacs-evil
+(prelude-require-packages '(treemacs treemacs-evil
                             treemacs-icons-dired treemacs-persp treemacs-projectile))
 
 ;; Enable all follow modes (so Treemacs always shows current file/project)
@@ -11,8 +11,11 @@
               treemacs-project-follow-mode t
               treemacs-width 25)
 ;; enable treemacs by default
-(when (fboundp 'treemacs)
+(with-eval-after-load 'treemacs
+  ;;(message "DEBUG POINT: %s" treemacs-persist-file)
   (treemacs)
   )
+
+(require 'treemacs)
 
 (provide 'my-treemacs)
