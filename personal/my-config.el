f1fc0f3803cbb720f8f28e26b60bebdd3ff5ed8f
Shylock Hg <tcath2s@icloud.com>
2025-12-02 15:36:43 +0800

    feat: reconfig c-mode

--- personal/my-config.el
+++ personal/my-config.el
@@ -17,4 +17,16 @@
   )
 
 
+(defun my-c-mode-common-defaults ()
+  (setq c-default-style "gnu"
+        c-basic-offset 2)
+  (c-set-offset 'substatement-open 0))
+
+(setq my-c-mode-common-hook 'my-c-mode-common-defaults)
+
+;; this will affect all modes derived from cc-mode, like
+;; java-mode, php-mode, etc
+(add-hook 'c-mode-common-hook (lambda ()
+                                (run-hooks 'my-c-mode-common-hook)) 100)
+
 (provide 'my-config)
