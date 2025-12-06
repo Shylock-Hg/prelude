0e5faab402a6914af3d93c07fb9e62db940b820d
Shylock Hg <tcath2s@icloud.com>
2025-12-06 16:16:50 +0800

    fix: treemacs and modules load order

--- personal/my-treemacs.el
+++ personal/my-treemacs.el
@@ -3,30 +3,49 @@
 (require 'prelude-packages)
 
 (prelude-require-packages '(treemacs treemacs-evil
-                            treemacs-persp treemacs-projectile nerd-icons treemacs-nerd-icons))
+                            treemacs-persp treemacs-projectile
+                            nerd-icons treemacs-nerd-icons
+                            treemacs-magit))
+
+
+;; Enable all follow modes (so Treemacs always shows current file/project)
+(use-package treemacs
+  :ensure t
+)
+
+(setq treemacs-follow-mode t
+      treemacs-tag-follow-mode t
+      treemacs-project-follow-mode t
+      treemacs-width 25)
 
 (use-package nerd-icons
   :ensure t
   :demand t
   )
 (use-package treemacs-nerd-icons
-  :after nerd-icons
+  :after (nerd-icons treemacs)
   :ensure t
   :demand t
   :config
   (treemacs-nerd-icons-config))
 
-;; Enable all follow modes (so Treemacs always shows current file/project)
-(setq-default treemacs-follow-mode t
-              treemacs-tag-follow-mode t
-              treemacs-project-follow-mode t
-              treemacs-width 25)
-;; enable treemacs by default
-(with-eval-after-load 'treemacs
-  ;;(message "DEBUG POINT: %s" treemacs-persist-file)
-  (treemacs)
-  )
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
 
-(require 'treemacs)
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+;;(use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
+  ;;:after (treemacs persp-mode) ;;or perspective vs. persp-mode
+  ;;:ensure t
+  ;;:config (treemacs-set-scope-type 'Perspectives))
+
+(treemacs-start-on-boot)
 
 (provide 'my-treemacs)
