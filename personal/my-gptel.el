4930dd4293b2738c607f374558fb406c06416530
Shylock Hg <tcath2s@icloud.com>
2025-12-06 22:38:05 +0800

    feat: gptel

---
+++ personal/my-gptel.el
@@ -0,0 +1,26 @@
+;;; -*- lexical-binding: t -*-
+
+(require 'prelude-packages)
+
+(prelude-require-package 'gptel)
+
+(use-package gptel
+  :ensure t
+  :demand t
+  )
+
+(require 'gptel)
+
+(setq gptel-api-key #'gptel-api-key-from-auth-source)
+(setq
+  gptel-model 'gemini-2.5-pro-exp-03-25
+  gptel-backend (gptel-make-gemini "Gemini"
+                  :key 'gptel-api-key-from-auth-source
+                  :stream t))
+;;(setq gptel-model 'grok-3-latest
+      ;;gptel-backend
+      ;;(gptel-make-xai "xAI"               ; Any name you want
+        ;;:key 'gptel-api-key-from-auth-source
+        ;;:stream t))
+
+(provide 'my-gptel)
