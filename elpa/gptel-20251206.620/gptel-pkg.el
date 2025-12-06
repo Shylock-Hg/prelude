f0c31cb9047b7a484bf49194d72ee5e34639fdd1
Shylock Hg <tcath2s@icloud.com>
2025-12-06 22:38:05 +0800

    feat: gptel

---
+++ elpa/gptel-20251206.620/gptel-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "gptel" "20251206.620"
+  "Interact with ChatGPT or other LLMs."
+  '((emacs     "27.1")
+    (transient "0.7.4")
+    (compat    "30.1.0.0"))
+  :url "https://github.com/karthink/gptel"
+  :commit "5ea747af3370ae9937b69042b43e50139cce73c0"
+  :revdesc "5ea747af3370"
+  :keywords '("convenience" "tools")
+  :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainers '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")))
