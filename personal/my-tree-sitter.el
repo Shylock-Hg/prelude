c6a7ce55a90a5f959a18134d3d693b14059b0ef9
Shylock Hg <tcath2s@icloud.com>
2025-12-11 17:21:14 +0800

    feat: provide function to install vendor repo

--- personal/my-tree-sitter.el
+++ personal/my-tree-sitter.el
@@ -1,17 +1,15 @@
 ;;; -*- lexical-binding: t -*-
 
 (require 'treesit)
+(require 'my-vendor)
 
-(defvar my-init-dir (file-name-directory user-init-file))
+(defvar my-tree-sitter-typst-src (my-vendor-dest "uben0" "tree-sitter-typst"))
+(defvar my-tree-sitter-cmake-src (my-vendor-dest "uyha" "tree-sitter-cmake"))
+(defvar my-tree-sitter-rust-src (my-vendor-dest "tree-sitter" "tree-sitter-rust"))
 
-(defvar my-tree-sitter-typst-src (expand-file-name "vendor/uben0/tree-sitter-typst" my-init-dir))
-(defvar my-tree-sitter-cmake-src (expand-file-name "vendor/uyha/tree-sitter-cmake" my-init-dir))
-
-(defvar my-tree-sitter-rust-src (expand-file-name "vendor/tree-sitter/tree-sitter-rust" my-init-dir))
-(defvar my-tree-sitter-rust-repo "https://github.com/tree-sitter/tree-sitter-rust.git")
-(when (not (file-directory-p my-tree-sitter-rust-src))
-  (call-process "git" nil nil nil "clone" "--depth=1" my-tree-sitter-rust-repo my-tree-sitter-rust-src)
-  )
+(my-vendor-install-git "github.com" "uben0" "tree-sitter-typst")
+(my-vendor-install-git "github.com" "uyha" "tree-sitter-cmake")
+(my-vendor-install-git "github.com" "tree-sitter" "tree-sitter-rust")
 
 (with-eval-after-load 'treesit
   (add-to-list 'treesit-language-source-alist
