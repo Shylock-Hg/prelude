728f5b21b9b6f6305261797884916e674c227f2d
Shylock Hg <tcath2s@icloud.com>
2025-12-11 20:19:10 +0800

    feat: reader

---
+++ personal/my-reader.el
@@ -0,0 +1,24 @@
+;;; -*- lexical-binding: t -*-
+
+(require 'my-vendor)
+(require 'use-package)
+
+(defvar my-reader-fresh-installed (my-vendor-install-git "codeberg.org" "divyaranjan" "emacs-reader"))
+
+(defvar my-reader-dir (my-vendor-dest "divyaranjan" "emacs-reader"))
+
+(when (eq my-reader-fresh-installed t)
+  (message "Compiling emacs-reader...")
+  (let ((ec (call-process "make" nil nil nil "-j8" "-C" my-reader-dir "all" "autoloads")) )
+    (when (not  (zerop ec))
+      (error "Compile emacs reader failed.")
+      )
+    )
+)
+
+(add-to-list 'load-path my-reader-dir)
+
+(require 'reader)
+(require 'reader-autoloads)
+
+(provide 'my-reader)
