e0e1642c6bc3c0efe6377324e5e94f59d2eca88c
Shylock Hg <tcath2s@icloud.com>
2026-01-02 10:39:21 +0800

    feat: change key binding of copilot-accept-completion to <insert-state>C-<return>

--- personal/my-github-copilot.el
+++ personal/my-github-copilot.el
@@ -9,9 +9,14 @@
 ;; M-x copilot-mode to enable copilot
 
 ;; github copilot
-(global-set-key (kbd "C-<return>") 'copilot-accept-completion)
+;;(global-set-key (kbd "C-<return>") 'copilot-accept-completion)
 
 ;; It's unique for each buffer, so we enable it in prog-mode-hook
 (add-hook 'prog-mode-hook 'copilot-mode)
+
+(add-hook 'copilot-mode-hook
+          (lambda ()
+            (define-key evil-insert-state-local-map
+                        (kbd "C-<return>") #'copilot-accept-completion)))
 
 (provide 'my-github-copilot)
