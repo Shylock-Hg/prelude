79b90adb7de8c85e0909818bf48d3702d40b6c7b
Shylock Hg <tcath2s@icloud.com>
2026-01-26 21:47:19 +0800

    feat: config gptel for zhipu GLM.

--- personal/my-gptel.el
+++ personal/my-gptel.el
@@ -11,12 +11,27 @@
 
 (require 'gptel)
 
-(setq gptel-api-key #'gptel-api-key-from-auth-source)
+;; from auth source by default
+;;(setq gptel-api-key #'gptel-api-key-from-auth-source)
+
+;;(setq
+  ;;gptel-model 'gemini-2.5-flash
+  ;;gptel-backend (gptel-make-gemini "Gemini"
+                  ;;:key 'gptel-api-key-from-auth-source
+                  ;;:stream t))
+
 (setq
-  gptel-model 'gemini-2.5-flash
-  gptel-backend (gptel-make-gemini "Gemini"
-                  :key 'gptel-api-key-from-auth-source
-                  :stream t))
+  gptel-model 'glm-4-flash
+  gptel-backend
+  (gptel-make-openai "Zhipu GLM"
+      :host "open.bigmodel.cn"                  ; Zhipu API host
+      :endpoint "/api/paas/v4/chat/completions" ; Chat endpoint
+      :stream t                                 ; Enable streaming responses
+      :key #'gptel-api-key-from-auth-source     ; Secure key retrieval (from .authinfo)
+      ;; :key (getenv "ZHIPU_API_KEY")
+      :models '("glm-4-flash"))
+  )
+
 ;;(setq gptel-model 'grok-3-latest
       ;;gptel-backend
       ;;(gptel-make-xai "xAI"               ; Any name you want
