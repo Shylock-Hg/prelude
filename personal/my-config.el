d0df09da01d924111becb58c22751050998e370e
Shylock Hg <tcath2s@icloud.com>
2026-02-07 10:33:28 +0800

    feat: config font of transient.

--- personal/my-config.el
+++ personal/my-config.el
@@ -2,6 +2,38 @@
 
 (set-face-attribute 'default nil :font "JetBrains Mono-20")
 
+(with-eval-after-load 'transient
+  (add-hook 'transient-setup-buffer-hook (lambda ()
+                                           (buffer-face-set '(:family "JetBrains Mono" :height 100))
+                                           )
+    )
+)
+
+;;(with-eval-after-load 'transient
+  ;;(set-face-attribute 'transient-key nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-key-stay nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-key-exit nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-key-noop nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-key-stack nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-key-return nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-nonstandard-key nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-mismatched-key nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-key-recurse nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-active-infix nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-heading nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-value nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-argument nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-inactive-argument nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-delimiter nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-higher-level nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-inapt-suffix nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-inapt-argument nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-unreachable nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-enabled-suffix nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-inactive-value nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-disabled-suffix nil :font "JetBrains Mono-12")
+  ;;(set-face-attribute 'transient-unreachable-key nil :font "JetBrains Mono-12")
+  ;;)
 
 ;; auto run emacs server
 (require 'server)
