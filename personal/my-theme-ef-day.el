a2e0dc2079a9e735555ab5a7c88e5ae18ab4ff4b
Shylock Hg <tcath2s@icloud.com>
2026-02-14 11:10:53 +0800

    minor: change theme.

--- personal/my-theme-ef-day.el
+++ personal/my-theme-ef-day.el
@@ -29,6 +29,6 @@
   ;; Finally, load your theme of choice (or a random one with
   ;; `modus-themes-load-random', `modus-themes-load-random-dark',
   ;; `modus-themes-load-random-light').
-  (modus-themes-load-theme 'ef-day))
+  (modus-themes-load-theme 'ef-melissa-light))
 
 (provide 'my-theme-ef-day)
