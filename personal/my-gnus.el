96659d9285cc35da5f2c2b5ba1d9dbb8563a72ba
Shylock Hg <tcath2s@icloud.com>
2026-02-24 21:11:01 +0800

    feat: config gnus.

---
+++ personal/my-gnus.el
@@ -0,0 +1,19 @@
+(setq user-full-name "Shylock Hg")
+(setq user-mail-address "tcath2s@icloud.com")
+
+;; ========== （IMAP）==========
+(setq gnus-select-method
+      '(nnimap "icloud"
+               (nnimap-address "imap.mail.me.com")
+               (nnimap-server-port 993)
+               (nnimap-stream ssl)
+               (nnimap-authinfo-file "~/.authinfo")))
+
+;; ========== （SMTP）==========
+(setq smtpmail-smtp-server "smtp.mail.me.com")
+(setq smtpmail-smtp-service 587)
+;;(setq settpmail-stream-type 'starttls)  ; iCloud use STARTTLS
+(setq send-mail-function 'smtpmail-send-it)
+(setq message-send-mail-function 'smtpmail-send-it)
+
+(provide 'my-gnus)
--- personal/my-modules.el
+++ personal/my-modules.el
@@ -32,3 +32,4 @@
 
 ;; tools
 (require 'my-reader)
+(require 'my-gnus)
